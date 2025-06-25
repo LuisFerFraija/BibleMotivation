207557eddaf4880b5ffac889cf72eb44807e6a97
luisff <luisf.fraija@gmail.com>
2025-06-24 22:15:53 -0400

    Initial Commit

---
+++ BibleMotivation10/BibleMotivation10App.swift
@@ -0,0 +1,17 @@
+//
+//  BibleMotivation10App.swift
+//  BibleMotivation10
+//
+//  Created by Luis Fernando on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BibleMotivation10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BibleMotivation10Tests/BibleMotivation10Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BibleMotivation10Tests.swift
+//  BibleMotivation10Tests
+//
+//  Created by Luis Fernando on 6/24/25.
+//
+
+import Testing
+@testable import BibleMotivation10
+
+struct BibleMotivation10Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
